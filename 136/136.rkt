0e9f519aae6b9791c40482a538d6850c6a7cbf6f
Doug <45773821+notdb@users.noreply.github.com>
2020-12-15 21:44:33 -0500

    add list to racket

--- 136/136.rkt
+++ 136/136.rkt
@@ -1,11 +1,13 @@
 #lang racket
-;; test comment
+; test comment
+;; Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+;; Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
 
 (define (showNum stuff)
-  (write 'hi)
-  (writeln ' ')
-  )
+  (writeln stuff))
 
 (define stuff 3)
+(define bigArray (list 1 2 3 4))
 
-(showNum stuff)
+(showNum bigArray)
