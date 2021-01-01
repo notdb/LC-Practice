53766476fd35c3c56df6f68292ac6a2577622b07
Doug <45773821+notdb@users.noreply.github.com>
2021-01-01 18:43:05 -0500

    add another line to Greet (starting Ada 136)

---
+++ 136/ada/greet.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO;
+
+procedure Greet is
+begin
+  -- Print "Hello, World!" to the screen
+  Ada.Text_IO.Put_line ("Hello, World!");
+  Ada.Text_IO.Put_line ("Hello, world (two)!");
+end Greet;
