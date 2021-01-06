c619d8a609daee0fcbd393d01aebd6886341768f
Doug <45773821+notdb@users.noreply.github.com>
2021-01-06 10:18:23 -0500

    update ada loop test

--- 136/ada/greet_5b.adb
+++ 136/ada/greet_5b.adb
@@ -2,4 +2,15 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Greet_5b is
    I : Integer := 1; -- Variable declaration
-   --   ^ Type    ^ Initial value
+   --   ^ Type    
+   --             ^ Initial value
+   begin
+      loop
+         Put_Line ("Hello, World!" & Integer'Image (I));
+	 exit when I = 5; -- Exit statement
+	 --        ^ Boolean condition
+
+         -- Assignment
+	 I := I + 1; -- There is no i++ short form to increment a variable
+      end loop;
+end Greet_5b;
