93ed47ca78d7948048c397e5d7f0da7bcda86134
Doug <45773821+notdb@users.noreply.github.com>
2021-01-11 09:17:35 -0500

    finish initial ada tutorial

---
+++ 136/ada/greet2.adb
@@ -0,0 +1,21 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Greet2 is
+begin
+    loop
+       Put_Line ("Please enter your name: ");
+
+       declare
+          Name : String := Get_Line;
+	  --               ^ Call to the Get_Line function
+       begin
+	  exit when Name = "";
+	  Put_line ("Hi " & Name & "!");
+
+       end;
+
+     -- Name is undefined here
+     end loop;
+
+    Put_Line ("Bye!");
+end Greet2;
